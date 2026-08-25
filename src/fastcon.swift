1732f886913a487578066be33aa3378cfdbbb47e
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:39:38 +0300

    Update fastcon.swift

--- src/fastcon.swift
+++ src/fastcon.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Fastcon{
     private let api = "https://fastcon.harknmav.fun/api"
     private var headers: [String: String]
